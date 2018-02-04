92fab50a8d75578f2bee4140cbe6bed912b45a94
saeefShovon <saeef.sheba@gmail.com>
2018-02-04 17:37:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/scrollabletabview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.user.scrollabletabview;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button allServicesBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        allServicesBtn = findViewById(R.id.allServicesBtn);
+        allServicesBtn.setOnClickListener(listener);
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.allServicesBtn:
+                    startActivity(new Intent(MainActivity.this, ScrollableTabsActivity.class));
+            }
+        }
+    };
+}
