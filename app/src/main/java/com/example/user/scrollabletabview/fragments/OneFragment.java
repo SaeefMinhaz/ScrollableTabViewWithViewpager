2cbc47ccbb3d8b055d302181c0a568ac7a6a9fe5
saeefShovon <saeef.sheba@gmail.com>
2018-02-05 13:23:40 +0600

    load all data in a single fragment

--- app/src/main/java/com/example/user/scrollabletabview/fragments/OneFragment.java
+++ app/src/main/java/com/example/user/scrollabletabview/fragments/OneFragment.java
@@ -5,26 +5,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.user.scrollabletabview.R;
 
 
 public class OneFragment extends Fragment {
 
-    public OneFragment() {
-        // Required empty public constructor
+    private String title;
+    private int page;
+    TextView pageTitleTV;
+
+    // newInstance constructor for creating fragment with arguments
+    public static OneFragment newInstance(int page, String title){
+            OneFragment oneFragment = new OneFragment();
+            Bundle args = new Bundle();
+            args.putInt("someInt", page);
+            args.putString("someTitle", title);
+            oneFragment.setArguments(args);
+
+            return oneFragment;
     }
 
+
+    // Store instance variables based on arguments passed
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
     }
 
+    // Inflate the view for the fragment based on layout XML
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        pageTitleTV = (TextView) view.findViewById(R.id.pageTitleTV);
+        pageTitleTV.setText(page + "" + title);
+
+        return view;
     }
 
 }
